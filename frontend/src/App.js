cbda9cf3c6e65e8c2780a906e6bfc30c68a9f05e
rino2000 <rino.rh35@gmail.com>
2023-04-01 23:56:18 +0200

    init

---
+++ frontend/src/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function Home() {
+  const [fileData, setFileData] = useState("");
+  const [predict, setPredict] = useState("");
+
+  const getFile = (e) => {
+    setFileData(e.target.files[0]);
+  };
+
+  const uploadFile = async (e) => {
+
+    e.preventDefault();
+
+    const data = new FormData();
+    data.append("file", fileData);
+
+    await axios({
+      method: "POST",
+      url: "http://localhost:5000/predict",
+      data: data,
+    }).then((res) => {
+      setPredict(JSON.stringify(res.data));
+    });
+  };
+
+  return (
+    <>
+      <form onSubmit={uploadFile}>
+        <input type="file" name="file" onChange={getFile} required />
+        <button type="submit">Upload </button>
+      </form>
+      {predict.length > 0 && <h2>Prediction = {predict.substring(2, 4)}</h2>}
+    </>
+  );
+}
